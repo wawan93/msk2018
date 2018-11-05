ad77ec89c9be858db945f66d3f129354789069b6
Vladimir Kochergin <v.kochergin@raftlab.ru>
2018-11-05 19:16:57 +0300

    новая ссылка

--- resources/views/debaters/list.blade.php
+++ resources/views/debaters/list.blade.php
@@ -19,7 +19,7 @@
                 </p>
                 </div>
                 <div class="col-md-6">
-                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/4QENZf1RhuI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/fVLSg7iPNfQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
