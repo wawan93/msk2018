6d5cf370b89bbc42729ba29049e662a2c65cc4df
Vladimir Kochergin <v.kochergin@raftlab.ru>
2018-11-05 18:14:54 +0300

    организаторы

---
+++ resources/views/info/organizators.blade.php
@@ -0,0 +1,17 @@
+@extends("debaters.layout")
+
+@section("content")
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-md-12">
+                <h3>По организационным вопросам</h3>
+                <p>Анна Кириченко - <a href="https://t.me/roboanna">@roboanna</a></p>
+                <p>Иван - <a href="https://t.me/willbefree">@willbefree</a></p>
+
+                <h3>По техническим вопросам</h3>
+                <p>Кочергин Владимир - <a href="https://t.me/wawan2030">@wawan2030</a></p>
+                <p>Илья Сухоруков - <a href="https://t.me/Suhofruktov">@Suhofruktov</a></p>
+            </div>
+        </div>
+    </div>
+@endsection
