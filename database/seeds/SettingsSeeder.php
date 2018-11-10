c8face7925bced2f94abd414f80efd8ef8a0fc00
Vladimir Kochergin <v.kochergin@raftlab.ru>
2018-11-10 17:27:25 +0300

    embed

---
+++ database/seeds/SettingsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('settings')->insert([
+            'key' => "embed",
+            'value' => "OlN3o88ULAA",
+        ]);
+    }
+}
--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -5,7 +5,7 @@
             Мы приглашаем вас стать участниками и зрителями Дебатного турнира, в ходе которого будут высказаны и&nbsp;услышаны самые разные точки зрения о будущем Московского отделения.
         </p>
         <div>
-            <iframe class="video" width="100%" height="100%" src="https://www.youtube.com/embed/M5urXTUQSzE" frameborder="0"
+            <iframe class="video" width="100%" height="100%" src="https://www.youtube.com/embed/{{ $embed }}" frameborder="0"
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
 
