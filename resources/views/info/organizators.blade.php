b17831c4001a29f84eef91243776c699b40e8634
Vladimir Kochergin <v.kochergin@raftlab.ru>
2018-11-11 21:51:44 +0300

    fix organizators links

--- resources/views/info/organizators.blade.php
+++ resources/views/info/organizators.blade.php
@@ -5,12 +5,12 @@
         <div class="row py-5">
             <div class="col-md-12">
                 <h3>По организационным вопросам</h3>
-                <p>Анна Кириченко - <a href="tg://resolve/roboanna">@roboanna</a></p>
-                <p>Иван Климов - <a href="tg://resolve/willbefree">@willbefree</a></p>
+                <p>Анна Кириченко - <a href="tg://resolve/?domain=roboanna">@roboanna</a></p>
+                <p>Иван Климов - <a href="tg://resolve/?domain=willbefree">@willbefree</a></p>
 
                 <h3>По техническим вопросам</h3>
-                <p>Владимир Кочергин - <a href="tg://resolve/wawan2030">@wawan2030</a></p>
-                <p>Илья Сухоруков - <a href="tg://resolve/Suhofruktov">@Suhofruktov</a></p>
+                <p>Владимир Кочергин - <a href="tg://resolve/?domain=wawan2030">@wawan2030</a></p>
+                <p>Илья Сухоруков - <a href="tg://resolve/?domain=Suhofruktov">@Suhofruktov</a></p>
             </div>
         </div>
     </div>
