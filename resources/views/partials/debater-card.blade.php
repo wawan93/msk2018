8969b59330eb1a48567f17829688115360188137
Vladimir Kochergin <v.kochergin@raftlab.ru>
2018-11-13 20:15:35 +0300

    костыль для двух победителей

--- resources/views/partials/debater-card.blade.php
+++ resources/views/partials/debater-card.blade.php
@@ -1,4 +1,8 @@
-<div class="debater card m-2 @if(isset($day) && ($day->winner == $debater->id)) winner @endif">
+<div class="debater card m-2 @if(isset($day) && ($day->winner == $debater->id && $day->id != 15))
+        winner
+@endif @if(isset($day) && ($day->winner != $debater->id && $day->id == 15))
+winner
+        @endif">
     @if(isset($day) && $day['result' . ($i + 1)])
         <div class="result">{{ $day['result' . ($i + 1)] }}%</div>
     @endif
